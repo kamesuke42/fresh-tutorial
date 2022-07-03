00ba3d96d27856eb26d2bd1c3bde371ea5787a39
kamesuke42 <kamesuke42@kamesuke42.com>
2022-07-03 20:38:30 +0900

    [main] fix

--- routes/index.tsx
+++ routes/index.tsx
@@ -15,7 +15,6 @@ export default function Home() {
         Welcome to `fresh`. Try update this message in the ./routes/index.tsx
         file, and refresh.
       </p>
-      <Counter start={3} />
     </div>
   );
 }
